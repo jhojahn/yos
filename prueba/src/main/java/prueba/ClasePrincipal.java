24385170059a1b665fce2adf09f0d0253f916528
jhojahn <jhojahnb@gmail.com>
2024-06-03 22:38:22 +0200

    ve 1

---
+++ prueba/src/main/java/prueba/ClasePrincipal.java
@@ -0,0 +1,24 @@
+package prueba;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+public class ClasePrincipal {
+    private static  Logger miLogEnFich = LogManager.getLogger("ClasePrincipal");
+    private static  Logger miLogConsola= LogManager.getLogger("ERROR");
+    
+    public static void main(String[] args) {
+        System.out.println("hola 1");
+        miLogEnFich.info("Errores informativo  de clase principal");
+        miLogEnFich.error("Errores grave de clase principal");
+        miLogEnFich.debug("Errores grave de clase principal");
+        miLogEnFich.warn("Errores grave de clase principal");
+        miLogEnFich.fatal("Errores grave de clase principal");
+        miLogConsola.error("Error grave en consola");
+        System.out.println("hola 2");
+        try {
+            throw new Exception("Excepción de jmpl");
+        } catch (Exception e) {
+            miLogEnFich.error(e.getMessage());
+        }
+        System.out.println("hola 3");
+    }
+}
